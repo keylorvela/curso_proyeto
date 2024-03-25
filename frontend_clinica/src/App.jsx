610eaecff5f61edf118a91756fb90954a3d7eb2e
Randy <rfonriv@gmail.com>
2024-03-25 17:01:07 -0600

    hotfix

--- frontend_clinica/src/App.jsx
+++ frontend_clinica/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import styles from './App.module.css';
 import Home from 'src/views/Home.jsx';
 import About from 'src/views/About.jsx';
