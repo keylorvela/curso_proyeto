6f09df7341ea552f2079714d770872f9f4cb8081
keylorvelas <keyvala48@gmail.com>
2024-03-29 19:22:12 -0600

    ajustes about

--- frontend_clinica/src/views/About.jsx
+++ frontend_clinica/src/views/About.jsx
@@ -2,26 +2,27 @@ import React from 'react';
 import image from 'src/assets/Vector.svg';
 import styles from 'src/views/About.module.css';
 
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
 
 import img from 'src/assets/stock2.jpg'
 
 const About = () => {
     return (
         <>
-            <h1 className={styles.aboutTitles} style={{ fontSize: '96px' }}>Sobre nosotros</h1>
+            <h1 className={styles.aboutTitles} style={{ fontSize: '96px', marginLeft: '4%' }}>Sobre nosotros</h1>
+            <Container className='d-flex justify-content-center'>
 
-            <Row>
                 <Col sm={6}>
                     <h2 className={styles.aboutTitles}>Nuestra clínica</h2>
-                    <p className={styles.aboutDescription}>Más aquí...</p>
+                    <p className={styles.aboutDescription}>Más aquí............................................. info</p>
                 </Col>
-                <Col sm={6} style={{ paddingTop: '10%', paddingBottom: '1%' }}>
-                    <Image src={img} width={400} height={400} fluid className="mx-auto d-block" />
+                <Col sm={6}>
+                    <Image src={img} className="img-fluid" style={{ marginTop: '20%', marginLeft: '15%' }} />
                 </Col>
-            </Row>
+
+            </Container>
             <div className={styles.imgDiv}>
                 <img
                     className={styles.img}
@@ -29,14 +30,14 @@ const About = () => {
                     alt="Imagen"
                 />
             </div>
-            <Row>
+            <Container className='d-flex justify-content-center'>
                 <Col sm={6}>
                     <div className={styles.centerText}>
                         <h2 className={styles.aboutTitles} >Misión</h2>
 
                     </div>
                     <div className={styles.centerText}>
-                        <p className={styles.aboutDescription}>Más aquí...</p>
+                        <p className={styles.aboutDescription}>Más aquí..................................... info</p>
                     </div>
                 </Col>
                 <Col sm={6}>
@@ -44,10 +45,10 @@ const About = () => {
                         <h2 className={styles.aboutTitles} >Visión</h2>
                     </div>
                     <div className={styles.centerText}>
-                        <p className={styles.aboutDescription}>Más aquí...</p>
+                        <p className={styles.aboutDescription}>Más aquí..................................... info</p>
                     </div>
                 </Col>
-            </Row>
+            </Container>
             <div className={styles.imgDiv}>
                 <img
                     className={styles.img}
@@ -55,15 +56,15 @@ const About = () => {
                     alt="Imagen"
                 />
             </div>
-            <Row>
-                <Col sm={6} style={{ paddingTop: '10%', paddingBottom: '1%' }}>
-                    <Image src={img} width={400} height={400} fluid className="mx-auto d-block" />
+            <Container className='d-flex justify-content-center'>
+                <Col sm={6}>
+                    <Image src={img} className="img-fluid" style={{ marginTop: '5%', marginBottom: '5%', marginLeft: '-10%' }} />
                 </Col>
                 <Col sm={6}>
-                    <h2 className={styles.aboutTitles}>Academia</h2>
-                    <p className={styles.aboutDescription}>Más aquí...</p>
+                    <h2 className={styles.aboutTitles} style={{ marginLeft: '15%' }}>Academia</h2>
+                    <p className={styles.aboutDescription} style={{ marginLeft: '20%', marginRight: '-15%' }}>Más aquí......................................... info</p>
                 </Col>
-            </Row>
+            </Container>
         </>
     );
 };
