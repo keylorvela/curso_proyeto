403642e5dfab2088325ab062b4d7abfa67f88d91
Randy <rfonriv@gmail.com>
2024-03-31 21:03:48 -0600

    Merge about page See MainLayout.jsx for more detail Pending review

--- frontend_clinica/src/views/About.jsx
+++ frontend_clinica/src/views/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import image from 'src/assets/Vector.svg';
+import image2 from 'src/assets/Vector(1).svg';
 import styles from 'src/views/About.module.css';
 
 import Col from 'react-bootstrap/Col';
@@ -7,69 +8,75 @@ import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 
 
-import MainNavbar from 'src/components/MainNavbar.jsx';
+import MainLayout from 'src/components/MainLayout.jsx';
 
 import img from 'src/assets/stock2.jpg'
 
 function About() {
     return (
         <>
-            <MainNavbar/>
 
-            <h1 className={styles.aboutTitles} style={{ fontSize: '7vw', marginLeft: '4%' }}>Sobre nosotros</h1>
-            <Container className='d-flex justify-content-center'>
+            <MainLayout>
 
-                <Col sm={6}>
-                    <h2 className={styles.aboutTitles}>Nuestra clínica</h2>
-                    <p className={styles.aboutDescription}>Más aquí............................................. info</p>
-                </Col>
-                <Col sm={6}>
-                    <Image src={img} className="img-fluid" style={{ marginTop: '20%', marginLeft: '15%' }} />
-                </Col>
+                <div className={styles.main}>
 
-            </Container>
-            <div className={styles.imgDiv}>
-                <img
-                    className={styles.img}
-                    src={image}
-                    alt="Imagen"
-                />
-            </div>
-            <Container className='d-flex justify-content-center'>
-                <Col sm={6}>
-                    <div className={styles.centerText}>
-                        <h2 className={styles.aboutTitles} >Misión</h2>
+                    <h1 className={styles.aboutTitles} style={{ fontSize: '7vw', marginLeft: '4%' }}>Sobre nosotros</h1>
+                    <Container className='d-flex justify-content-center'>
 
+                        <Col sm={6}>
+                            <h2 className={styles.aboutTitles}>Nuestra clínica</h2>
+                            <p className={styles.aboutDescription}>Más aquí............................................. info</p>
+                        </Col>
+                        <Col sm={6}>
+                            <Image src={img} className="img-fluid" />
+                        </Col>
+
+                    </Container>
+                    <div className={styles.imgDiv}>
+                        <img
+                            className={styles.img}
+                            src={image}
+                            alt="Imagen"
+                        />
                     </div>
-                    <div className={styles.centerText}>
-                        <p className={styles.aboutDescription}>Más aquí..................................... info</p>
+                    <Container className='d-flex justify-content-center'>
+                        <Col sm={6}>
+                            <div className={styles.centerText}>
+                                <h2 className={styles.aboutTitles} >Misión</h2>
+
+                            </div>
+                            <div className={styles.centerText}>
+                                <p className={styles.aboutDescription}>Más aquí..................................... info</p>
+                            </div>
+                        </Col>
+                        <Col sm={6}>
+                            <div className={styles.centerText}>
+                                <h2 className={styles.aboutTitles} >Visión</h2>
+                            </div>
+                            <div className={styles.centerText}>
+                                <p className={styles.aboutDescription}>Más aquí..................................... info</p>
+                            </div>
+                        </Col>
+                    </Container>
+                    <div className={styles.imgDiv}>
+                        <img
+                            className={styles.img}
+                            src={image2}
+                            alt="Imagen"
+                        />
                     </div>
-                </Col>
-                <Col sm={6}>
-                    <div className={styles.centerText}>
-                        <h2 className={styles.aboutTitles} >Visión</h2>
-                    </div>
-                    <div className={styles.centerText}>
-                        <p className={styles.aboutDescription}>Más aquí..................................... info</p>
-                    </div>
-                </Col>
-            </Container>
-            <div className={styles.imgDiv}>
-                <img
-                    className={styles.img}
-                    src={image}
-                    alt="Imagen"
-                />
-            </div>
-            <Container className='d-flex justify-content-center'>
-                <Col sm={6}>
-                    <Image src={img} className="img-fluid" style={{ marginTop: '5%', marginBottom: '5%', marginLeft: '-10%' }} />
-                </Col>
-                <Col sm={6}>
-                    <h2 className={styles.aboutTitles} style={{ marginLeft: '15%' }}>Academia</h2>
-                    <p className={styles.aboutDescription} style={{ marginLeft: '20%', marginRight: '-15%' }}>Más aquí......................................... info</p>
-                </Col>
-            </Container>
+                    <Container className='d-flex justify-content-center'>
+                        <Col sm={6}>
+                            <Image src={img} className="img-fluid" />
+                        </Col>
+                        <Col sm={6}>
+                            <h2 className={styles.aboutTitles}>Academia</h2>
+                            <p className={styles.aboutDescription} >Más aquí......................................... info</p>
+                        </Col>
+                    </Container>
+                </div>
+
+            </MainLayout>
         </>
     );
 };
