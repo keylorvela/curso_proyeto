6faa523e5caf0511d7ce8daba889549e02b154d4
DeynerNB <deynernavarrob@gmail.com>
2024-04-02 15:37:08 -0600

    Change password Fixed

--- Scripts - DB/Store Procedures/Login/SP_Login_Change_Password.sql
+++ Scripts - DB/Store Procedures/Login/SP_Login_Change_Password.sql
@@ -9,6 +9,9 @@ CREATE PROCEDURE SP_Login_Change_Password(
 BEGIN
     -- Declare control variable
     DECLARE v_UserCount INT;
+    DECLARE v_currentOldPassword VARCHAR(256);
+    DECLARE v_insertedOldPassword VARCHAR(256);
+    DECLARE v_salt VARCHAR(256);
     
     -- Check if the user exist
     SELECT COUNT(*)
@@ -19,12 +22,26 @@ BEGIN
     IF @v_UserCount = 0 THEN
         SET o_status = 'Error: User not found.';
     ELSE
-        -- Update user password
-        UPDATE User
-            SET Password = p_newPassword
-            WHERE ID = p_userID;
+		-- Get the salt and current password
+        SELECT
+			U.Password, U.Salt
+		INTO
+			@v_currentOldPassword, @v_salt
+		FROM `User` AS U
+        WHERE U.ID = p_userID;
         
-        SET o_status = "Success: User updated";
+        -- Compare passwords
+        SET @v_insertedOldPassword = SHA2(CONCAT(p_oldPassword, @v_salt), 256);
+        IF @v_currentOldPassword = @v_insertedOldPassword THEN
+			SET o_status = "Error: Passwords not match";
+		ELSE
+			 -- Update user password
+			UPDATE User
+				SET Password = SHA2(CONCAT(p_newPassword, @v_salt), 256)
+				WHERE ID = p_userID;
+                
+			SET o_status = "Success: User updated";
+        END IF;
     END IF;
 END //
 
