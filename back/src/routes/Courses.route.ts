8317a04f74965d039aad51e844957704baedf884
DeynerNB <deynernavarrob@gmail.com>
2024-04-07 17:06:34 -0600

    Added course route

--- back/src/routes/Courses.route.ts
+++ back/src/routes/Courses.route.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
+import { getCourseList, createCourse, updateCourse, deleteCourse, dropOutCourse, listEnrolledCourses, searchCourse } from "../controllers/Courses.controller";
+
 const router = Router();
 
-router.get("/", async (req, res) => {
-    res.status(200).send("Hola");
-})
+// Rutas para la gestión de cursos
+router.get("/", getCourseList);
+router.post("/", createCourse);
+router.put("/:courseId", updateCourse);
+router.delete("/:courseId", deleteCourse);
+router.get("/:courseId", searchCourse)
+
+// Rutas para la gestión de la participación en cursos
+router.post("/:courseId/drop-out", dropOutCourse);
+router.get("/enrolled/:userID", listEnrolledCourses);
 
 export default router;
