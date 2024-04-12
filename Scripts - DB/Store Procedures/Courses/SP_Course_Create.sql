55f81470ee3abd5812df97b07be60b95896252ad
DeynerNB <deynernavarrob@gmail.com>
2024-04-11 20:39:43 -0600

    Added transaction

--- Scripts - DB/Store Procedures/Courses/SP_Course_Create.sql
+++ Scripts - DB/Store Procedures/Courses/SP_Course_Create.sql
@@ -13,22 +13,35 @@ CREATE PROCEDURE SP_Course_Create(
 )
 BEGIN
     DECLARE v_courseCount INT;
+    DECLARE v_transactionStatus INT;
 
     SELECT COUNT(*)
         INTO @v_courseCount
         FROM Course AS C
         WHERE C.Name = p_name;
 
+	-- Start transaction process
+    START TRANSACTION;
+
     IF (@v_courseCount > 0) THEN
         SET o_status = "Error: Course repeated";
+        SET v_transactionStatus = 0;
     ELSE
         INSERT INTO Course (Name, Description, Price)
             VALUES (p_name, p_description, p_price);
 
         INSERT INTO bqhd9nbafrpsvzpzrgvc.Group(StartingDate, ScheduleDate, ScheduleHour, Capacity, CourseID)
-            VALUES (p_startingDate, p_date, p_hour, p_capacity, @LAST_INSERT_ID());
+            VALUES (p_startingDate, p_date, p_hour, p_capacity, LAST_INSERT_ID());
 
         SET o_status = "Success: Course created";
+        SET v_transactionStatus = 1;
+    END IF;
+
+    -- Commit or rollback transaction based on status
+    IF v_transactionStatus = 1 THEN
+        COMMIT;
+    ELSE
+        ROLLBACK;
     END IF;
 
     SELECT o_status;
