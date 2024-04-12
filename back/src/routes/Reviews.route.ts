51bfd3378fa23862e77094b07b8f26eeea294c8b
DeynerNB <deynernavarrob@gmail.com>
2024-04-11 21:30:16 -0600

    Update review routes

--- back/src/routes/Reviews.route.ts
+++ back/src/routes/Reviews.route.ts
@@ -2,8 +2,12 @@ import { addReview, removeReview } from "../controllers/Reviews.controller";
 import { Router } from "express";
 const router = Router();
 
-router.post("/", addReview)
+router.post("/", addReview);
 
-router.post("/", removeReview)
+router.delete("/", removeReview);
+
+router.get("/", (req, res) => {res.status(200).json({saludo: "Desde get Review :)"})});
+
+router.put("/", (req, res) => {res.status(200).json({saludo: "Desde put Review :)"})});
 
 export default router;
