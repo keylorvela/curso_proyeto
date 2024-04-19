346ce506fa8b436adafd464c264b37db22f5ed0c
hdgr33 <hdgr@estudiantec.cr>
2024-04-18 20:25:35 -0600

    Update Teachers.controller.ts

--- back/src/controllers/Teachers.controller.ts
+++ back/src/controllers/Teachers.controller.ts
@@ -2,19 +2,21 @@ import { Request, Response } from "express";
 import { RowDataPacket } from "mysql2";
 import dbConnection from "../database/dbConfig";
 import { OStatus } from "../interfaces/OStatus.interface";
+import { Professor } from "interfaces/Professor.interface";
 
 export const getAllProfessors = async (req: Request, res: Response) => {
     try {
         const result = await dbConnection.query<RowDataPacket[]>(`
             CALL SP_Professors_ReadAll()
         `);
-        const professorsList: any[] = JSON.parse(JSON.stringify(result[0]));
+        const professorsList: Professor[] = JSON.parse(JSON.stringify(result[0]));
 
         res.status(200).send(professorsList || []);
     } catch (error) {
         res.status(400).send({ error: "Request Failed", info: error });
     }
 }
+
 
 export const updateProfessor = async (req: Request, res: Response) => {
     const { personID, photo, email, phoneNumber, name }: { personID: number; photo: any; email: string; phoneNumber: string; name: string } = req.body;
@@ -29,7 +31,12 @@ export const updateProfessor = async (req: Request, res: Response) => {
             CALL SP_Professors_Update(${personID}, "${photo}", "${email}", "${phoneNumber}", "${name}")
         `);
 
-        res.status(200).send({ success: true });
+        const result = await dbConnection.query<RowDataPacket[]>(`
+            SELECT @o_status AS o_status
+        `);
+
+        const resultStatus: OStatus[] = JSON.parse(JSON.stringify(result[0]));
+        res.status(200).send(resultStatus[0] || {});
     } catch (error) {
         res.status(400).send({ error: "Request Failed", info: error });
     }
@@ -48,7 +55,12 @@ export const deleteProfessor = async (req: Request, res: Response) => {
             CALL SP_Professors_Delete(${userID})
         `);
 
-        res.status(200).send({ success: true });
+        const result = await dbConnection.query<RowDataPacket[]>(`
+            SELECT @o_status AS o_status
+        `);
+
+        const resultStatus: OStatus[] = JSON.parse(JSON.stringify(result[0]));
+        res.status(200).send(resultStatus[0] || {});
     } catch (error) {
         res.status(400).send({ error: "Request Failed", info: error });
     }
