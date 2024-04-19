aa0bc04b78dac2f26d20ccc30ad68347f08cc207
keylorvelas <keyvala48@gmail.com>
2024-04-18 22:35:58 -0600

    dynamic table v1

--- frontend_admin/src/App.jsx
+++ frontend_admin/src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Treatments from 'src/views/Treatments.jsx';
 import ModifyTreatment from 'src/views/ModifyTreatment.jsx';
+import Professors from 'src/views/Professors.jsx';
 import Login from 'src/views/Login.jsx';
 
 function App() {
   return (
     <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<Professors />} />
             <Route path="treatments" element={<Treatments />} />
             <Route path="modifyTreatment/:id" element={<ModifyTreatment />} />
+            <Route path="professors" element={<Professors />} />
           </Routes>
     </BrowserRouter>
   );
