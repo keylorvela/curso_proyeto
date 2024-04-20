1965835ea21b280e28727ee703f10a6c438235e2
DeynerNB <deynernavarrob@gmail.com>
2024-04-19 18:45:40 -0600

    General interface

---
+++ back/src/interfaces/General/Register.interface.ts
@@ -0,0 +1,9 @@
+export interface RegisterBody {
+    p_name: String,
+    p_email: String,
+    p_phone_number: String,
+    p_photo: String,
+    p_username: String,
+    p_password: String,
+    p_type: String
+}
