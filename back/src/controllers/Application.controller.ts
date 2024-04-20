169ca1febf0677d070bb03ab19da00700013a835
DeynerNB <deynernavarrob@gmail.com>
2024-04-19 18:37:18 -0600

    Send Application function

--- back/src/controllers/Application.controller.ts
+++ back/src/controllers/Application.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { RowDataPacket } from "mysql2";
 import dbConnection from "../database/dbConfig";
 import { OStatus } from "../interfaces/OStatus.interface";
-import { Application } from "interfaces/Application.interface";
+import { ApplicationBody } from "../interfaces/Application.interface";
 
 
 export const getAllApplications = async (req: Request, res: Response) => {
@@ -10,7 +10,7 @@ export const getAllApplications = async (req: Request, res: Response) => {
         const result = await dbConnection.query<RowDataPacket[]>(`
             CALL SP_Application_ReadAll()
         `);
-        const applicationsList: Application[] = JSON.parse(JSON.stringify(result[0]));
+        const applicationsList: ApplicationBody[] = JSON.parse(JSON.stringify(result[0]));
 
         res.status(200).send(applicationsList || []);
     } catch (error) {
@@ -19,25 +19,41 @@ export const getAllApplications = async (req: Request, res: Response) => {
 }
 
 
+export const respondToApplication = async (req: Request, res: Response) => {
+    const { applicationID, status }: { applicationID: number; status: string } = req.body;
 
-
-export const deleteTreatment = async(req: Request, res: Response) => {
-    const str_treatmentID = req.params.treatmentID;
-    const treatmentID: number | null = Number(str_treatmentID) || null;
-
-    // Check if id is a valid input
-    if (!treatmentID || treatmentID < 0) {
-        res.status(400).send({ error: "Treatment id must be a valid number" });
+    if (isNaN(applicationID) || applicationID < 0 || !status) {
+        res.status(400).send({ error: "Invalid input provided" });
         return;
     }
 
     try {
-        const result_treatment = await dbConnection.query<RowDataPacket[]>(`CALL SP_Treatment_Delete(${treatmentID}, @o_status)`);
-        const result: OStatus[] = JSON.parse(JSON.stringify(result_treatment[0][0]));
+        await dbConnection.query<RowDataPacket[]>(`
+            CALL SP_Application_Respond(${applicationID}, "${status}", @o_status)
+        `);
+
+        res.status(200).send({ success: true });
+    } catch (error) {
+        res.status(400).send({ error: "Request Failed", info: error });
+    }
+}
+
+export const sendApplication = async (req: Request, res: Response) => {
+    const body: ApplicationBody = req.body;
+
+    if (body.email == "" || body.name == "" || body.payment_receipt == "" || body.phone_number == "" || isNaN(body.groupID) || body.groupID <= 0) {
+        res.status(400).send({ error: "All fields are necessary" });
+        return;
+    }
+
+    try {
+        const result_application = await dbConnection.query<RowDataPacket[]>(`SP_Application_Send("${body.name}", "${body.payment_receipt}", "${body.email}", "${body.phone_number}", ${body.groupID}, @o_status)`);
+        const result: OStatus[] = JSON.parse(JSON.stringify(result_application[0][0]));
 
         res.status(200).send(result[0] || {});
     } catch (error) {
         res.status(500).send({ error: "Petition failed", error_detail: error });
     }
-};
+
+}
 
