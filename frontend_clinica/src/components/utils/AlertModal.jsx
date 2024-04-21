a91a2ab4343a9af420252a94caaffa3d2a3e7bed
Randy <rfonriv@gmail.com>
2024-04-21 16:13:59 -0600

    Fix some minor details

--- frontend_clinica/src/components/utils/AlertModal.jsx
+++ frontend_clinica/src/components/utils/AlertModal.jsx
@@ -2,9 +2,21 @@ import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 
 import Alert from 'react-bootstrap/Alert';
-/*
 
-  const [showAlert, setShowAlert] = useState(false);
+
+
+/**
+ * Component to render when loading
+ *
+ * @component
+ * @param {string} title - Alert's title
+ * @param {string} message - Alert's content
+ * @param {boolean} showAlert - Alert's state, should be false by default
+ * @param {function} setShowAlert - Handler function for the state
+ * @returns {JSX.Element} The rendered loading component.
+ *
+ * @example
+ *   const [showAlert, setShowAlert] = useState(false);
 
   setShowAlert(true);
 
@@ -15,11 +27,9 @@ import Alert from 'react-bootstrap/Alert';
                   showAlert={showAlert}
                   setShowAlert={setShowAlert}
                 />
-
-*/
-
-
-function AlertModal({ title, message, showAlert, setShowAlert }) {
+   
+* */
+function AlertModal({ title, message, showAlert=false, setShowAlert }) {
   const handleClose = () => setShowAlert(false);
 
   return (
--- frontend_clinica/src/services/treatmentsService.js
+++ frontend_clinica/src/services/treatmentsService.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 const API_BASE_URL = 'https://backend-proyeto.vercel.app';
 
-
+/**
+ * Obtiene todos los tratamientos desde la API.
+ * @returns {Promise} Una promesa que resuelve con los datos de los tratamientos o null si hay un error.
+ */
 export async function getTreatments() {
   try {
     const response = await axios.get(`${API_BASE_URL}/treatments`);
@@ -13,7 +16,11 @@ export async function getTreatments() {
   }
 }
 
-
+/**
+ * Obtiene un tratamiento específico desde la API.
+ * @param {number} id_treatment - El ID del tratamiento que se desea obtener.
+ * @returns {Promise} Una promesa que resuelve con los datos del tratamiento o null si hay un error.
+ */
 export async function getTreatment(id_treatment) {
   try {
     const response = await axios.get(`${API_BASE_URL}/treatments/${id_treatment}`);
