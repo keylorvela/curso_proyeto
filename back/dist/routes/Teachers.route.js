5d52c8b04dca92079554c22d1b7ac82897a48ea9
DeynerNB <deynernavarrob@gmail.com>
2024-04-21 13:57:01 -0600

    dist

--- back/dist/routes/Teachers.route.js
+++ back/dist/routes/Teachers.route.js
@@ -6,8 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const Teachers_controller_1 = require("../controllers/Teachers.controller");
 const router = express_1.default.Router();
-router.get("/all", Teachers_controller_1.getAllProfessors);
-router.put("/update", Teachers_controller_1.updateProfessor);
-router.delete("/delete/:userID", Teachers_controller_1.deleteProfessor);
+router.get("/", Teachers_controller_1.getAllProfessors);
+router.put("/", Teachers_controller_1.updateProfessor);
+router.delete("/:userID", Teachers_controller_1.deleteProfessor);
 exports.default = router;
 //# sourceMappingURL=Teachers.route.js.map
