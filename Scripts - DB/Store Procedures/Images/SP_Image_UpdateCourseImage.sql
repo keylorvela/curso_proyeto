77680eb08cc31bfc617f48a283d5ace49e96d248
DeynerNB <deynernavarrob@gmail.com>
2024-04-25 19:59:56 -0600

    Added new SP

---
+++ Scripts - DB/Store Procedures/Images/SP_Image_UpdateCourseImage.sql
@@ -0,0 +1,48 @@
+DELIMITER //
+
+CREATE PROCEDURE SP_Image_UpdateCourseImage(
+    IN p_imageID INT,
+    IN p_imageURL VARCHAR(64),
+
+    OUT o_status VARCHAR(32)
+)
+BEGIN
+    -- Declare control variable
+    DECLARE v_imageCount INT;
+    DECLARE v_transactionStatus INT;
+
+    -- Check if the Image exists
+    SELECT COUNT(*)
+        INTO @v_imageCount
+        FROM CourseImage AS CI
+        WHERE CI.ID = p_imageID;
+
+    -- Start transaction
+    START TRANSACTION;
+
+    -- Error if the Image does not exists
+    IF @v_imageCount <= 0 THEN
+        SET o_status = "Error: Course Image NOT found";
+        SET v_transactionStatus = 0;
+    ELSE
+        -- Update the image url
+        UPDATE CourseImage
+            SET
+                ImageUrl = p_imageURL
+            WHERE ID = p_imageID;
+
+        SET o_status = "Success: Course Image updated";
+        SET v_transactionStatus = 1;
+    END IF;
+
+    -- Commit or rollback transaction based on status
+    IF v_transactionStatus = 1 THEN
+        COMMIT;
+    ELSE
+        ROLLBACK;
+    END IF;
+
+    SELECT o_status;
+END //
+
+DELIMITER ;
---
+++ Scripts - DB/Store Procedures/Login/SP_Login_Verify_Email.sql
@@ -0,0 +1,64 @@
+DELIMITER //
+
+CREATE PROCEDURE SP_Login_Verify_Email(
+    IN p_userID INT,
+    IN p_newPassword VARCHAR(64),
+
+    OUT o_status VARCHAR(32)
+)
+BEGIN
+    -- Declare control variable
+    DECLARE v_UserCount INT;
+    DECLARE v_currentOldPassword VARCHAR(256);
+    DECLARE v_insertedOldPassword VARCHAR(256);
+    DECLARE v_salt VARCHAR(256);
+    DECLARE v_transactionStatus INT;
+
+    -- Check if the user exist
+    SELECT COUNT(*)
+        INTO @v_UserCount
+        FROM User AS U
+        WHERE U.ID = p_userID;
+
+    -- Start transaction
+	START TRANSACTION;
+
+    IF @v_UserCount = 0 THEN
+        SET o_status = 'Error: User not found.';
+        SET v_transactionStatus = 0;
+    ELSE
+		-- Get the salt and current password
+        SELECT
+			U.Password, U.Salt
+		INTO
+			@v_currentOldPassword, @v_salt
+		FROM `User` AS U
+        WHERE U.ID = p_userID;
+
+        -- Compare passwords
+        SET @v_insertedOldPassword = SHA2(CONCAT(p_oldPassword, @v_salt), 256);
+        IF @v_currentOldPassword = @v_insertedOldPassword THEN
+			SET o_status = "Error: Passwords not match";
+            SET v_transactionStatus = 0;
+		ELSE
+			 -- Update user password
+			UPDATE User
+				SET Password = SHA2(CONCAT(p_newPassword, @v_salt), 256)
+				WHERE ID = p_userID;
+
+			SET o_status = "Success: User updated";
+            SET v_transactionStatus = 1;
+        END IF;
+    END IF;
+
+    -- Commit or rollback transaction based on status
+    IF v_transactionStatus = 1 THEN
+        COMMIT;
+    ELSE
+        ROLLBACK;
+    END IF;
+
+    SELECT o_status;
+END //
+
+DELIMITER ;
