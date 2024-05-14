a7e7159f23dbda9966028b1d1bf4cb8d59281922
keylorvelas <keyvala48@gmail.com>
2024-05-13 18:02:29 -0600

    front CRUD categories

--- frontend_admin/src/components/FeaturedTreatments.jsx
+++ frontend_admin/src/components/FeaturedTreatments.jsx
@@ -1,4 +1,4 @@
-import {React,useState, useEffect} from 'react';
+import { React, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import styles from 'src/components/FeaturedTreatments.module.css'
@@ -9,52 +9,47 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import img from 'src/assets/stock2.jpg'
-import TreatmentsService from 'src/services/Treatments.service.js'; 
+import TreatmentsService from 'src/services/Treatments.service.js';
 
 function FeaturedTreatments() {
 
-    const navegate = useNavigate ();
+    const navegate = useNavigate();
     const [treatments, setTreatments] = useState([]);
 
     useEffect(() => {
         const getTreatments = async () => {
             try {
-              const treatmentsData = await TreatmentsService.getTreatments();
-              setTreatments(treatmentsData);
+                const treatmentsData = await TreatmentsService.getTreatments();
+                setTreatments(treatmentsData);
             } catch (error) {
-              console.error('getTreatments fail:', error);
+                console.error('getTreatments fail:', error);
             }
-          };
-          getTreatments();
-      }, []);
+        };
+        getTreatments();
+    }, []);
 
 
     const handleButton = (treatmentInfo) => {
         navegate('/modifyTreatment/' + treatmentInfo.Name, {
-        state: {treatmentInfo}
+            state: { treatmentInfo }
         });
     };
 
     return (
         <>
             <main className={styles.body}>
-                <Container className='d-flex justify-content-end gap-3'>
-                    <Row>
-                        {treatments?.map(tratamiento => (
-                            <Col sm={3} key={tratamiento.ID}>
-                                <Treatment
-                                    photo={img}
-                                    treatmentInfo={tratamiento} 
-                                    event={handleButton} 
-                                    className={styles.treatment} 
-                                />
-                            </Col>
-                        ))}
-
-
-                    </Row>
-                </Container>
-
+                <Row>
+                    {treatments?.map(tratamiento => (
+                        <Col sm={3} key={tratamiento.ID}>
+                            <Treatment
+                                photo={img}
+                                treatmentInfo={tratamiento}
+                                event={handleButton}
+                                className={styles.treatment}
+                            />
+                        </Col>
+                    ))}
+                </Row>
             </main>
         </>
     );
--- frontend_admin/src/views/admin/Treatments.jsx
+++ frontend_admin/src/views/admin/Treatments.jsx
@@ -1,28 +1,162 @@
-import MainLayout from 'src/components/MainLayout.jsx'
-import FeaturedTreatments from 'src/components/FeaturedTreatments.jsx'
-import styles from 'src/views/admin/Treatments.module.css';
-
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 import { useNavigate } from 'react-router-dom';
+import { Table } from 'react-bootstrap';
+
+import MainLayout from 'src/components/MainLayout.jsx';
+import FeaturedTreatments from 'src/components/FeaturedTreatments.jsx';
+import styles from 'src/views/admin/Treatments.module.css';
+import BaseModal from 'src/components/utils/BaseModal.jsx';
+import DynamicForm from 'src/components/DynamicForm.jsx';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 
 
 function Treatments() {
 
-    const navegate = useNavigate();
+    const [categories, setCategories] = useState([{ ID: 1, name: "categoria1" }, { ID: 2, name: "categoria2" }]);
+    const [newCategoryName, setNewCategoryName] = useState('');
+    const [editedCategory, setEditedategory] = useState({});
+    const [showTable, setShowTable] = useState(false);
+    const [modalShow, setModalShow] = useState(false);
 
+    const navigate = useNavigate();
     const handleRedirect = () => {
-        navegate('/modifyTreatment/new');
+        navigate('/modifyTreatment/new');
+    };
+
+    const fields = [
+        {
+            id: 'name',
+            label: 'Nombre de categoría:',
+            type: 'text',
+            placeholder: 'Ingresa el nombre de categoría',
+            required: true,
+        },
+    ];
+    const buttons = [{ variant: 'primary', type: 'submit', label: 'Editar' }]
+
+    const handleEditCategory = async (data) => {
+        console.log(data);
+        setModalShow(false);
+        const updatedCategories = [...categories];
+        updatedCategories.forEach(category => {
+            if (category.ID === data.ID) {
+                category.name = data.name;
+            }
+        });
+        setCategories(updatedCategories);
+    }
+
+    const handleAddCategory = () => {
+        if (newCategoryName.trim() !== '') {
+            console.log(newCategoryName);
+            setCategories([...categories, { ID: categories.length + 1, name: newCategoryName }]);
+            setNewCategoryName('');
+        }
+    };
+
+    const handleDeleteCategory = (index) => {
+        console.log(index);
+        const updatedCategories = categories.filter(category => category.ID !== index);
+        setCategories(updatedCategories);
     };
 
     return (
-        <MainLayout type = {1}>
-            <h1 className={styles.title}>Tratamientos</h1>
-            <div className={styles.line}></div>
-            <div className="d-flex justify-content-end">
-                <Button variant="primary" className={styles.customButton} onClick={handleRedirect}>Añadir Tratamiento</Button>
-            </div>
-            <div className={styles.line}></div>
-            <FeaturedTreatments></FeaturedTreatments>
+        <MainLayout type={1}>
+
+            <BaseModal
+                pshow={modalShow}
+                setShow={setModalShow}
+            >
+                <div className='px-3'>
+                    <DynamicForm
+                        fields={fields}
+                        onSubmit={handleEditCategory}
+                        buttons={buttons}
+                        initialValues={editedCategory}
+                    />
+                </div>
+            </BaseModal>
+
+
+            <Container fluid style={{ width: '98%' }}>
+                <h1 className={styles.title}>Tratamientos</h1>
+                <div className={styles.line}></div>
+                <div className="d-flex justify-content-end">
+                    <Button variant="primary"
+                        className={styles.customButton}
+                        onClick={handleRedirect}>
+                        Añadir Tratamiento
+                    </Button>
+                </div>
+                <div className={styles.line}></div>
+                <FeaturedTreatments></FeaturedTreatments>
+
+
+                {/* Category Section */}
+                <div className={styles.categorySection}>
+                    <h2 className={styles.categoryTitle}>Gestionar Categorías</h2>
+
+                    <div className={styles.categoryAdd} >
+                        <input
+                            className={styles.input}
+                            type="text" value={newCategoryName}
+                            onChange={(e) => setNewCategoryName(e.target.value)}
+                            placeholder="Nueva categoría"
+                        />
+                        <Button className={styles.btn}
+                            variant="outline-primary"
+                            onClick={handleAddCategory}>
+                            <FontAwesomeIcon icon={faPlus} />
+                            Agregar
+                        </Button>
+                    </div>
+
+
+                    <Button onClick={() => setShowTable(!showTable)}>
+                        {showTable ? 'Ocultar categorías' : 'Mostrar categorías'}
+                    </Button>
+                    {showTable && (
+                        <Table striped hover responsive borderless >
+                            <thead>
+                                <tr>
+                                    <th>Categoría</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {categories.map((category, index) => (
+                                    <tr key={index}>
+                                        <td>{category.name}</td>
+                                        <td>
+                                            <Button className={styles.btn}
+                                                variant="outline-primary"
+                                                size="sm"
+                                                onClick={() => {
+                                                    setEditedategory(category);
+                                                    setModalShow(true);
+                                                }} >
+                                                <FontAwesomeIcon icon={faEdit} />
+                                                Editar
+                                            </Button>
+                                            <Button variant="outline-danger"
+                                                size="sm"
+                                                onClick={() => handleDeleteCategory(category.ID)}>
+                                                <FontAwesomeIcon icon={faTrash} />
+                                                Eliminar
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                    )}
+                </div>
+            </Container>
         </MainLayout>
     );
 }
