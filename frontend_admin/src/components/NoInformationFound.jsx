ebacff8e6e279402afd7177559c225953635f8f9
DeynerNB <deynernavarrob@gmail.com>
2024-05-16 15:27:43 -0600

    Added no information component

---
+++ frontend_admin/src/components/NoInformationFound.jsx
@@ -0,0 +1,11 @@
+
+function NoInformationFound({ message }) {
+    return (
+        <div style={{ marginTop: "40px", textAlign: "center" }}>
+            <span style={{ fontSize: "1.2rem", color: "#545454" }}>{message}</span>
+        </div>
+    );
+}
+
+
+export default Treatment;
