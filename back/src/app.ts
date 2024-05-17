0d054db8341ae8b19b6938dee21e6014d9982acf
Randy <rfonriv@gmail.com>
2024-05-17 16:29:01 -0600

    hotfix

--- back/src/app.ts
+++ back/src/app.ts
@@ -20,6 +20,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: "15mb" }));
+app.use(express.urlencoded({ extended: true }));
 
 // General routes
 // ---------------------------------------------
