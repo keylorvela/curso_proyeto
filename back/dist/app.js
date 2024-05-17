ac146566715f5c77644589ee8cf6633b180193dd
Randy <rfonriv@gmail.com>
2024-05-17 16:22:48 -0600

    dist

--- back/dist/app.js
+++ back/dist/app.js
@@ -20,6 +20,7 @@ const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json({ limit: "15mb" }));
+app.use(express_1.default.urlencoded({ extended: true }));
 // General routes
 // ---------------------------------------------
 app.use("/register", Register_route_1.default);
