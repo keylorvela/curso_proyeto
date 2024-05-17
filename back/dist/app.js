9703bd4dfe912b78d507353842a47f89a8574750
Randy <rfonriv@gmail.com>
2024-05-17 17:19:21 -0600

    dist

--- back/dist/app.js
+++ back/dist/app.js
@@ -28,7 +28,7 @@ app.use(express_1.default.json({ limit: "15mb" }));
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5174');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
