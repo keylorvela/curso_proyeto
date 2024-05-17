e72bf25aadac0311b06a1abd43acadb06d3b5607
DeynerNB <deynernavarrob@gmail.com>
2024-05-17 14:52:15 -0600

    Cards information component

---
+++ frontend_clinica/src/components/CardsInformation.jsx
@@ -0,0 +1,46 @@
+import styles from 'src/components/CardsInformation.module.css';
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function CardsInformation({ titles, contents }) {
+    return (
+        <>
+        <Row className={styles.cardsMainContainer}>
+            {
+            titles.map(
+                (title, index) => (
+                    contents[index]
+                    &&
+                    <>
+                        <Col key={index} className={styles.informationCard}>
+                            <Row>
+                                <Col>
+                                    <h3 className="fs-2 fw-semibold my-3" style={{ color: "var(--main-blue)" }}>{title}</h3>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    {
+                                        (contents[index].includes('/')) ? (
+                                            contents[index].split('/').map((value, subIndex) => (
+                                                <p className="fs-4" key={`${index}-${subIndex}`}>{value}</p>)
+                                            )
+                                        ) : (
+                                            <p className="fs-4" key={`${index}-${0}`}>{contents[index]}</p>
+                                        )
+                                    }
+                                </Col>
+                            </Row>
+                        </Col>
+                        <hr className={styles.divider}></hr>
+                    </>
+                )
+            )
+            }
+        </Row>
+        </>
+    )
+}
+
+export default CardsInformation;
