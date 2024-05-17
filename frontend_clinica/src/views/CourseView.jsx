27ca554fcba88c0d2448a397bab7fc5bd6b9d0cc
DeynerNB <deynernavarrob@gmail.com>
2024-05-17 14:52:54 -0600

    Added cards information component

--- frontend_clinica/src/views/CourseView.jsx
+++ frontend_clinica/src/views/CourseView.jsx
@@ -1,109 +1,115 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+
 import Alert from 'react-bootstrap/Alert'; // Agrega la importación de Alert
 import Loading from 'src/components/utils/Loading.jsx';
+
 import styles from 'src/views/CourseView.module.css';
+import commonStyles from 'src/components/Common.module.css';
+
 import MainLayout from 'src/components/MainLayout.jsx';
 import CourseForm from 'src/components/CourseForm.jsx';
+
 import sinpe from 'src/assets/Sinpe.svg';
 import transferencia from 'src/assets/Transferencia.svg';
+import img from 'src/assets/stock2.jpg';
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
-import img from 'src/assets/stock2.jpg';
 import Button from 'react-bootstrap/Button';
 import Badge from 'react-bootstrap/Badge';
+
 import { FaWhatsapp } from "react-icons/fa";
 import { FiShoppingCart } from "react-icons/fi";
 import { getCourse } from 'src/services/coursesService.js';
 
 function CourseView() {
-  const { id } = useParams();
-  const [course, setCourse] = useState(null);
-  const [loading, setLoading] = useState(true);
+    const { id } = useParams();
+    const [course, setCourse] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await getCourse(id);
-        setCourse(result);
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching course:', error);
-        setLoading(false); // Aquí debes manejar el error adecuadamente según tu caso de uso
-      }
-    }
-    fetchData();
-  }, [id]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await getCourse(id);
+                setCourse(result);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching course:', error);
+                setLoading(false); // Aquí debes manejar el error adecuadamente según tu caso de uso
+            }
+        }
+        fetchData();
+    }, [id]);
 
-  return (
-    <MainLayout>
-      <div className={styles.page}>
-        <Container>
-          {loading ? (
-            <div className='text-center'>
-              <Loading size={15} />
+    return (
+        <MainLayout>
+            <div className={styles.page}>
+                <Container>
+                    {loading ? (
+                        <div className='text-center'>
+                            <Loading size={15} />
+                        </div>
+                    ) : (
+                        /* En caso de que no se encuentre el curso */
+                        Object.keys(course).length === 0 ? (
+
+                            <Alert key='warning' variant='warning'>
+                                No se encontró el curso.
+                            </Alert>
+                        ) : (
+                            <>
+                                <Row className="pb-5 mb-5">
+                                    <Col className={styles.title} xs={12}>
+                                        <h2 className={`text-center fs-1 fw-semibold my-3`}>{course?.Name}</h2>
+                                    </Col>
+                                </Row>
+                                <Row className='mt-5'>
+                                    <Col className="align-self-center" xs={12} md={4}>
+                                        <Image src={img} fluid />
+                                    </Col>
+                                    <Col xs={12} md={8}>
+                                        <p className="fs-3">{course?.explicacion}</p>
+                                        <p className="fs-3">{course?.Description}</p>
+                                        <p className="fs-3">{course?.Information}</p>
+                                    </Col>
+                                </Row>
+
+                                <Row className="mt-3">
+                                    <Col className="my-5" xs={12}>
+                                        <h3 className="fs-1 mb-3" style={{ color: "var(--main-blue)" }}>Métodos de pago:</h3>
+                                        <br />
+                                        <p className="fs-3">Ofrecemos los siguientes métodos de pago: transferencia bancaria, Sinpe Móvil y link de pago directo con Conexión BP. ¿Listo para realizar tu compra? ¡Contáctanos por WhatsApp y agenda tu cita hoy mismo! </p>
+                                    </Col>
+                                </Row>
+
+                                <Row className="mb-4">
+                                    <Col xs={12} sm={6} lg={3}>
+                                        <button className={`d-flex align-items-center flex-nowrap justify-content-center gap-1 px-3 py-3 btn mb-2 btn-primary btn-lg ${commonStyles.wtsButton}`}>
+                                            <FaWhatsapp /><b className="text-nowrap"> Agenda tu cita </b>
+                                        </button>
+                                    </Col>
+                                    <Col xs={12} sm={6} lg={3}>
+                                        <button className={`d-flex align-items-center flex-nowrap justify-content-center gap-1 px-3 py-3 btn mb-2 btn-primary btn-lg ${commonStyles.bpButton}`}>
+                                            <FiShoppingCart /><b className="text-nowrap"> Conexión BP</b>
+                                        </button>
+                                    </Col>
+                                    <Col xs={6} sm={6} lg={3}>
+                                        <Image src={sinpe} fluid />
+                                    </Col>
+                                    <Col xs={6} sm={6} lg={3}>
+                                        <Image src={transferencia} fluid />
+                                    </Col>
+                                </Row>
+                            </>
+                        )
+                    )}
+                </Container>
             </div>
-          ) : (
-            /* En caso de que no se encuentre el curso */
-            Object.keys(course).length === 0 ? (
-              
-              <Alert key='warning' variant='warning'>
-                No se encontró el curso.
-              </Alert>
-            ) : (
-              <>
-                <Row className="pb-5 mb-5">
-                  <Col className={styles.title} xs={12}>
-                    <h2 className={`text-center fs-1 fw-semibold my-5`}>{course?.Name}</h2>
-                  </Col>
-                </Row>
-                <Row className='mt-5'>
-                  <Col className="align-self-center" xs={12} md={4}>
-                    <Image src={img} fluid />
-                  </Col>
-                  <Col xs={12} md={8}>
-                    <p className="fs-3">{course?.explicacion}</p>
-                    <p className="fs-3">{course?.Description}</p>
-                    <p className="fs-3">{course?.Information}</p>
-                  </Col>
-                </Row>
-                <Row className="mt-5">
-                  <Col className="my-5" xs={12}>
-                    <h3 className="fs-1 mb-3">Métodos de pago:</h3>
-                    <p className="fs-3">Ofrecemos los siguientes métodos de pago: transferencia bancaria, Sinpe Móvil y link de pago directo con Conexión BP. ¿Listo para realizar tu compra? ¡Contáctanos por WhatsApp y agenda tu cita hoy mismo!</p>
-                  </Col>
-                </Row>
-                <Row className='d-flex text-center justify-content-center mb-3'>
-                  <Col>
-                    <button className={`px-3 py-3 btn btn-primary btn-lg ${styles.wtsButton}`}>
-                      <FaWhatsapp className="me-3" /><b> Agenda tu cita </b>
-                    </button>
-                  </Col>
-                  <Col>
-                    <button className={`px-3 py-3 btn btn-primary btn-lg ${styles.bpButton}`}>
-                      <FiShoppingCart className="me-3" /><b> Conexión BP</b>
-                    </button>
-                  </Col>
-                  <Col>
-                    <Image src={sinpe} width={125} height={125} />
-                  </Col>
-                  <Col>
-                    <Image src={transferencia} width={125} height={125} />
-                  </Col>
-                </Row>
-                <Row className='d-flex text-center justify-content-center my-3'>
-                  <Col>
-                  </Col>
-                </Row>
-              </>
-            )
-          )}
-        </Container>
-      </div>
-    </MainLayout>
-  );
+        </MainLayout>
+    );
 }
 
 export default CourseView;
--- frontend_clinica/src/views/TreatmentView.jsx
+++ frontend_clinica/src/views/TreatmentView.jsx
@@ -15,112 +15,121 @@ import Loading from 'src/components/utils/Loading.jsx';
 import styles from 'src/views/TreatmentView.module.css';
 import commonStyles from 'src/components/Common.module.css';
 
-
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import Alert from 'react-bootstrap/Alert';
 
-
-
-
+import CardsInformation from "../components/CardsInformation";
 
 function TreatmentView() {
-  const { id } = useParams();
-  const [treatment, setTreatment] = useState(null);
-  const [loading, setLoading] = useState(true);
+    const { id } = useParams();
+    const [treatment, setTreatment] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getTreatment(id);
-        setTreatment(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        setLoading(false);
-      }
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await getTreatment(id);
+                setTreatment(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, [id]);
+
+    const formatCurrency = (value) => {
+        return Intl.NumberFormat('es-CR', {
+            style: "currency",
+            currency: "CRC"
+        }).format(value);
     }
-    fetchData();
-  }, [id]);
 
-  return (
-    <MainLayout>
-      <div className={styles.page}>
-        <Container>
-          {loading ? (
-            <div className='text-center'>
-              <Loading size={15} />
+    return (
+        <MainLayout>
+            <div className={styles.page}>
+                <Container>
+                    {loading ? (
+                        <div className='text-center'>
+                            <Loading size={15} />
+                        </div>
+                    ) : (
+
+                        /* En caso de que no se encuentre el tratamiento */
+                        Object.keys(treatment).length === 0 ? (
+                            <Alert key='warning' variant='warning'>
+                                No se encontró el tratamiento.
+                            </Alert>
+                        ) : (
+
+                            <>
+                                {/* Diseño de tratamiento */}
+
+                                <Row >
+                                    <Col className={styles.title} >
+                                        <h2 className="text-center fs-1 fw-semibold my-3">{treatment?.Name}</h2>
+                                    </Col>
+                                </Row>
+                                <Row className='my-5'>
+                                    <Col className="align-self-center" xs={12} md={4}>
+                                        <Image src={img || treatment.TreatmentImage} fluid />
+                                    </Col>
+                                    <Col xs={12} md={8}>
+                                        <p className="fs-3">{treatment?.Description}</p>
+                                    </Col>
+                                </Row>
+
+                                {/* Content carousel */}
+                                <CardsInformation
+                                    titles={["Incluye:", "Información:"]}
+                                    contents={[treatment?.Includes, treatment?.Information]}
+                                />
+                                <CardsInformation
+                                    titles={["Duración del procedimiento:", "Duración del efecto:"]}
+                                    contents={[treatment?.ProcedureDuration, treatment?.EffectDuration]}
+                                />
+                                <CardsInformation
+                                    titles={["Precio:"]}
+                                    contents={[`${formatCurrency(treatment?.Price)}`]}
+                                />
+
+                                <Row className="mt-3">
+                                    <Col className="my-5" xs={12}>
+                                        <h3 className="fs-1 mb-3" style={{ color: "var(--main-blue)" }}>Métodos de pago:</h3>
+                                        <br />
+                                        <p className="fs-3">Ofrecemos los siguientes métodos de pago: transferencia bancaria, Sinpe Móvil y link de pago directo con Conexión BP. ¿Listo para realizar tu compra? ¡Contáctanos por WhatsApp y agenda tu cita hoy mismo! </p>
+                                    </Col>
+                                </Row>
+
+                                <Row className="mb-4">
+                                    <Col xs={12} sm={6} lg={3}>
+                                        <button className={`d-flex align-items-center flex-nowrap justify-content-center gap-1 px-3 py-3 btn mb-2 btn-primary btn-lg ${commonStyles.wtsButton}`}>
+                                            <FaWhatsapp/><b className="text-nowrap"> Agenda tu cita </b>
+                                        </button>
+                                    </Col>
+                                    <Col xs={12} sm={6} lg={3}>
+                                        <button className={`d-flex align-items-center flex-nowrap justify-content-center gap-1 px-3 py-3 btn mb-2 btn-primary btn-lg ${commonStyles.bpButton}`}>
+                                            <FiShoppingCart/><b className="text-nowrap"> Conexión BP</b>
+                                        </button>
+                                    </Col>
+                                    <Col xs={6} sm={6} lg={3}>
+                                        <Image src={sinpe} fluid />
+                                    </Col>
+                                    <Col xs={6} sm={6} lg={3}>
+                                        <Image src={transferencia} fluid />
+                                    </Col>
+                                </Row>
+                            </>
+                        )
+                    )}
+                </Container>
             </div>
-          ) : (
-            
-            /* En caso de que no se encuentre el tratamiento */
-            Object.keys(treatment).length === 0 ? (
-              <Alert key='warning' variant='warning'>
-                No se encontró el tratamiento.
-              </Alert>
-            ) : (
-
-              <>
-              {/* Diseño de tratamiento */}
-
-                <Row >
-                  <Col className={styles.title} xs={10}>
-                    <h2 className="text-center fs-1 fw-semibold my-3">{treatment?.Name}</h2>
-                  </Col>
-                </Row>
-                <Row className='mt-5'>
-                  <Col className="align-self-center" xs={12} md={4}>
-                    <Image src={img} fluid />
-                  </Col>
-                  <Col xs={12} md={8}>
-                    <p className="fs-3">{treatment?.llamativo}</p>
-                    <p className="fs-3">{treatment?.Description}</p>
-                    <p className="fs-3">{treatment?.Information}</p>
-                  </Col>
-                </Row>
-
-                {/* Content carousel */}
-                <Row>
-                  <Col></Col>
-                </Row>
-
-                <Row className="mt-5">
-                  <Col className="my-5" xs={12}>
-                    <h3 className="fs-1 mb-3">Métodos de pago:</h3>
-                    <br />
-                    <p className="fs-3">Ofrecemos los siguientes métodos de pago: transferencia bancaria, Sinpe Móvil y link de pago directo con Conexión BP. ¿Listo para realizar tu compra? ¡Contáctanos por WhatsApp y agenda tu cita hoy mismo! </p>
-                  </Col>
-                </Row>
-
-                <Row className='d-flex text-center justify-content-center mb-3'>
-                  <Col>
-                    <button className={`px-3 py-3 btn btn-primary btn-lg ${commonStyles.wtsButton}`}>
-                      <FaWhatsapp className="me-3" /><b> Agenda tu cita </b>
-                    </button>
-                  </Col>
-                  <Col>
-                    <button className={`px-3 py-3 btn btn-primary btn-lg ${commonStyles.bpButton}`}>
-                      <FiShoppingCart className="me-3" /><b> Conexión BP</b>
-                    </button>
-                  </Col>
-                  <Col>
-                    <Image src={sinpe} width={125} height={125} />
-                  </Col>
-                  <Col>
-                    <Image src={transferencia} width={125} height={125} />
-                  </Col>
-                </Row>
-
-                <Row className='d-flex text-center justify-content-center my-3'></Row>
-              </>
-            )
-          )}
-        </Container>
-      </div>
-    </MainLayout>
-  );
+        </MainLayout>
+    );
 }
 
 export default TreatmentView;
