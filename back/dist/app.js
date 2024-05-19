38d70ddf4ad8a01a311317890da86d24e4b952bd
Randy <rfonriv@gmail.com>
2024-05-19 14:39:32 -0600

    dist

--- back/dist/app.js
+++ back/dist/app.js
@@ -18,7 +18,10 @@ const Teachers_route_1 = __importDefault(require("./routes/Teachers.route"));
 const Treatments_route_1 = __importDefault(require("./routes/Treatments.route"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 app.use(express_1.default.json({ limit: "15mb" }));
 app.use(express_1.default.urlencoded({ extended: true }));
 // General routes
