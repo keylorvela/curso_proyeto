56dc63a67e9f2b10b27852bcf4b1cbbd18209a96
Randy <rfonriv@gmail.com>
2024-05-19 15:41:09 -0600

    dist

--- back/dist/routes/Applications.route.js
+++ back/dist/routes/Applications.route.js
@@ -7,20 +7,19 @@ const express_1 = __importDefault(require("express"));
 const multer_1 = __importDefault(require("multer"));
 const Application_controller_1 = require("../controllers/Application.controller");
 const router = express_1.default.Router();
-// Configuración de multer para guardar archivos
 const storage = multer_1.default.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, '/tmp'); // Directorio temporal
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`);
     },
 });
 const upload = (0, multer_1.default)({ storage });
+router.post("/", upload.single('file'), Application_controller_1.sendApplication);
 //Endpoints
 router.get("/", Application_controller_1.getAllApplications);
 router.put("/", Application_controller_1.respondToApplication);
-router.post("/", upload.single('file'), Application_controller_1.sendApplication);
 router.get("/file/:idApplication", Application_controller_1.getApplicationFile);
 router.get('/test', (req, res) => {
     res.send("aaHOLA");
