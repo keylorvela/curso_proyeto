a7990ab2a4d4ce933091257682c9af22379e8639
Randy Fonseca <49202815+RandyFonsec@users.noreply.github.com>
2024-05-25 11:25:58 -0600

    Update SP_Login_Start_Session.sql

--- Scripts - DB/Store Procedures/Login/SP_Login_Start_Session.sql
+++ Scripts - DB/Store Procedures/Login/SP_Login_Start_Session.sql
@@ -5,31 +5,43 @@ CREATE PROCEDURE SP_Login_Start_Session(
     IN p_password VARCHAR(64)
 )
 BEGIN
-    -- Declare the variable to store the user id
+    -- Declare variables to store user details
     DECLARE v_ID_User INT;
+    DECLARE v_Username VARCHAR(64);
+    DECLARE v_UserTypeID INT;
     DECLARE v_original_hash VARCHAR(256);
     DECLARE v_entry_hash VARCHAR(256);
     DECLARE v_salt VARCHAR(256);
 
-    -- Select the user id based on the params
+    -- Select user details based on the input username
     SELECT
         U.ID,
+        U.Username,
+        U.UserTypeID,
         U.Password,
         U.Salt
     INTO
-        @v_ID_User,
-        @v_original_hash,
-        @v_salt
+        v_ID_User,
+        v_Username,
+        v_UserTypeID,
+        v_original_hash,
+        v_salt
     FROM User AS U
     WHERE U.Username = p_user;
 
-    SET @v_entry_hash = SHA2(CONCAT(p_password, @v_salt), 256);
+    SET v_entry_hash = SHA2(CONCAT(p_password, v_salt), 256);
 
-    -- Return the user id found
-    IF @v_ID_User IS NOT NULL AND @v_entry_hash = @v_original_hash THEN
-        SELECT v_ID_User AS ID_User;
+    -- Return user details if the password matches
+    IF v_ID_User IS NOT NULL AND v_entry_hash = v_original_hash THEN
+        SELECT
+            v_ID_User AS ID,
+            v_Username AS Username,
+            v_UserTypeID AS UserTypeID;
     ELSE
-        SELECT NULL AS ID_User;
+        SELECT
+            NULL AS ID,
+            NULL AS Username,
+            NULL AS UserTypeID;
     END IF;
 
 END //
